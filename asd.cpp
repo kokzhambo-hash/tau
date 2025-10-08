3b82809059b26e8b7b410fb798c70a83d8e915cb
kokzhambo-hash <kokzhambo@gmail.com>
2025-10-08 10:55:32 +0500

    Add files via upload

---
+++ asd.cpp
@@ -0,0 +1,18 @@
+# include <iostream>
+using namespace std;
+int main(){
+    int arr[5];
+    cout <<"masiv ;";
+    for (int i = 0; i < 5; i++){
+        cin >> arr[i];
+    }
+    cout << "mas:";
+    for (int i =0; i<5; i++){
+        cout << arr[i]<<" ";
+    
+    }
+
+    cout << endl;
+    return 0;
+
+}
